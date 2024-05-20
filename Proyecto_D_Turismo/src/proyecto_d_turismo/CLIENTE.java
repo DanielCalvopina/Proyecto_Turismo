5ff9e1e4f4407807bca5773dbb2d64dae3056018
DanielCalvopina <148661526+DanielCalvopina@users.noreply.github.com>
2024-05-20 07:49:48 -0500

    wa
    Cliente

--- Proyecto_D_Turismo/src/proyecto_d_turismo/CLIENTE.java
+++ Proyecto_D_Turismo/src/proyecto_d_turismo/CLIENTE.java
@@ -1,4 +1,30 @@
 package proyecto_d_turismo;
+import java.util.LinkedHashMap;
 public class CLIENTE {
-    
+    //13 Caracteres
+    String Cedula;
+    //25 Caracteres
+    String Nombre;
+    //25 Caracteres
+    String Apellido;
+    //35 Caracteres
+    String Direccion;
+    public CLIENTE(String Cedula,String Nombre,String Apellido,String Direccion){
+        this.Cedula = Cedula;
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.Direccion = Direccion;
+    }
+    public String getCedula(){
+        return Cedula;
+    }
+    public String getNombre(){
+        return Nombre;
+    }
+    public String getApellido(){
+        return Apellido;
+    }
+    public String getDireccion(){
+        return Direccion;
+    }
 }
